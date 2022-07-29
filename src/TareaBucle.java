77d41d7fa4f19cabb049bc6db0a0ace605454732
Cristian Baez <106565940+Cristianpjc@users.noreply.github.com>
2022-07-29 17:52:44 -0400

    Tarea Bucle

---
+++ src/TareaBucle.java
@@ -0,0 +1,9 @@
+public class TareaBucle {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Cristian", "Selva", "Vero", "Cati", "Noah"};
+        for (int i = 0; i<= nombres.length;i++)
+
+        System.out.println(nombres [i]);
+    }
+}
